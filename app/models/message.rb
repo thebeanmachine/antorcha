e274a891539d3ce2088a605c84974408a90a28e7
Daan Oosterveld <daan@dusdanig.com>
2010-06-15 15:21:13 +0200

    Added step selection.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,8 @@
 class Message < ActiveRecord::Base
   validates_presence_of :title
+  validates_presence_of :step
+
+  belongs_to :step
   
   named_scope :incoming, :conditions => {:incoming => true}
   named_scope :outgoing, :conditions => {:incoming => false}
