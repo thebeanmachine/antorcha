10a8f2ca0c88a0e72dd4a042b19f8a6d949d5dab
Daan Oosterveld <daan@dusdanig.com>
2010-06-29 14:44:58 +0200

    Test voor worker verwijderd, ze zijn niet goed uitgewerkt.

--- spec/models/worker_spec.rb
+++ spec/models/worker_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
 describe Worker do
-  describe "finds delayed job workers" do
-    it "works" do
-      debug Worker.all.inspect
-    end
-  end
-  
-  describe "can create a new delayed_job" do
-    it "start" do
-      Worker.start
-    end
-  end
+  it "should be tested, but how?"
 end
