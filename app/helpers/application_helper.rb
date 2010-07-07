6af3dc574d674d7e673721d22afe5d0c0e041b91
Daan Oosterveld <daan@dusdanig.com>
2010-07-07 20:30:17 +0200

    Breadcrumb initial version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,49 @@ module ApplicationHelper
     values.join '' unless values.include? nil
   end
   
+  def crumble *models
+    models.inject [] do |memo, model|
+      last = Array(memo.last)
+      memo << last + [model.class] unless model.kind_of? Class
+      memo << last + [model] 
+    end
+  end
+  
+  def shallow_crumble paths
+    crumbles = paths.inject([]) {|m,p| m << crumble(*p) }
+    polynate(crumbles)
+  end
+
+  def polynate crumbles
+    crumbles.inject do |memo, path|
+      if path.first.last.kind_of? Class
+        memo << (memo.last + [path.shift.last])
+      end
+      memo += path
+    end
+  end
+  
+  def arrayize *models
+    models.inject [[]] do |memo, model|
+      if model.kind_of? Array
+        memo << model << []
+      else
+        memo.last << model
+      end
+      memo
+    end.reject &:blank?
+  end
+  
+  def breadcrumb *models
+    @content_for_breadcrumb = shallow_crumble(arrayize(*models)).collect do |route|
+      last = route.last
+      poly_path = route.map {|part| part.kind_of?(Class) ? part.name.underscore.pluralize.to_sym : part}
+      if last.kind_of? Class
+        link_to last.human_name(:count => 3), poly_path
+      else
+        link_to last.title, poly_path
+      end
+    end.join(" » ")
+  end
+  
 end
