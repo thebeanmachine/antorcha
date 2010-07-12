e42e2390281f03e5de4d0471a359fa60f464ff1d
Robin <robinjanse@gmail.com>
2010-07-12 11:43:08 +0200

    alter migration to exclude default devise modules

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100706131512) do
+ActiveRecord::Schema.define(:version => 20100712092312) do
 
   create_table "definitions", :force => true do |t|
     t.string   "title",      :null => false
@@ -116,5 +116,15 @@ ActiveRecord::Schema.define(:version => 20100706131512) do
     t.string   "uri"
     t.datetime "stopped_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                             :default => "", :null => false
+    t.string   "encrypted_password", :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                     :default => "", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
