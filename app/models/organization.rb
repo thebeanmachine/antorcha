c2982cdf5ca97c1798b7cca0e0bc4988a0ad7bcc
Robin <robinjanse@gmail.com>
2010-07-13 13:06:15 +0200

    Fulfills join for Organization and Role

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,6 +5,6 @@ class Organization < ActiveRecord::Base
   validates_presence_of :title
   validates_uniqueness_of :title
   
-  # has_many :fulfills
-  # has_many :roles, :through => :fulfills 
+  has_many :fulfills
+  has_many :roles, :through => :fulfills 
 end
