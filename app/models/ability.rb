571c38a2f1404a46b9c726c8602850cb3b4cd712
Robin <robinjanse@gmail.com>
2010-07-13 13:54:43 +0200

    Added some Users with Roles. CanCan is set to "manage :all" to make work pleasant for now.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,26 +2,27 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.roles.include? :maintainer
-      can :manage, :all
-      can :send, Message
-      cannot :manage, [Definition, Role, Step]
-      cannot :create, Role
-      cannot :update, Role
-    end
-    if user.roles.include? :advisor
-      can :manage, :all
-      cannot :manage, Worker
-      cannot :send, Message
-    end
-    if user.roles.include? :communicator
-      can :send, Message
-      can :create, Message
-      cannot :create, Role
-      cannot :update, Role
-      cannot :manage, [Definition, Step]
-    end
-    can :read, [Transaction, Message, Role]
-    can :create, [Transaction]
+    # if user.roles.include? :maintainer
+    #   can :manage, :all
+    #   can :send, Message
+    #   cannot :manage, [Definition, Role, Step]
+    #   cannot :create, Role
+    #   cannot :update, Role
+    # end
+    # if user.roles.include? :advisor
+    #   can :manage, :all
+    #   cannot :manage, Worker
+    #   cannot :send, Message
+    # end
+    # if user.roles.include? :communicator
+    #   can :send, Message
+    #   can :create, Message
+    #   cannot :create, Role
+    #   cannot :update, Role
+    #   cannot :manage, [Definition, Step]
+    # end
+    # can :read, [Transaction, Message, Role]
+    # can :create, [Transaction]
+    can :manage, :all
   end
 end
