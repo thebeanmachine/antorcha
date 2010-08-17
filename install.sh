24b4bcff53fe18b26084af56d89aaf1ce11b5d04
Maarten Brouwers <maarten@brouwers.me>
2010-08-17 18:55:14 +0200

    Installatie scripts vertaald... c'mon...

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 clear
-echo -e '\E[37;44m'"\033[1m *** Antorcha installation script *** \033[0m"
+echo -e '\E[37;44m'"\033[1m *** Antorcha installatie script *** \033[0m"
 echo
 echo
 
-echo -e "\033[1m Download Antorcha \033[0m"
+echo -e "\033[1m Antorcha downloaden \033[0m"
 echo
 git clone git@github.com:thebeanmachine/antorcha.git antorcha
 echo
-echo -e "\033[1m Done! \033[0m"
+echo -e "\033[1m Gereed! \033[0m"
 
 echo
 cd antorcha
@@ -17,7 +17,7 @@ git checkout origin/maint -b maint
 echo
 
 echo
-echo -e "\033[1m Download submodules \033[0m"
+echo -e "\033[1m Initialisatie submodules \033[0m"
 echo
 git submodule init
 
@@ -26,16 +26,16 @@ echo -e "\033[1m Done! \033[0m"
 echo
 
 echo
-echo -e "\033[1m Update submodules \033[0m"
+echo -e "\033[1m Bijwerken submodules \033[0m"
 echo
 git submodule update
 
 echo
-echo -e "\033[1m Done! \033[0m"
+echo -e "\033[1m Gereed! \033[0m"
 echo
 
 echo
-echo -e "\033[1m Setup Antorcha \033[0m"
+echo -e "\033[1m Database configureren en installeren \033[0m"
 echo
 RAILS_ENV=production rake db:create
 RAILS_ENV=production rake db:migrate
@@ -43,22 +43,15 @@ RAILS_ENV=production rake db:seed
 RAILS_ENV=production rake gems:install
 
 echo
-echo -e "\033[1m Done! \033[0m"
+echo -e "\033[1m Gereed! \033[0m"
 echo
 
-echo -e '\E[37;44m'"\033[1m *** Antorcha succesfull installed *** \033[0m"
+echo -e '\E[37;44m'"\033[1m *** Antorcha is geinstalleerd! *** \033[0m"
 echo
 
 echo 
-echo -e '\E[37;44m'"\033[1m Please go to http://localhost:3000, when the server is ready.\033[0m"
+echo -e '\E[37;44m'"\033[1m U dient nu uw webserver dusdanig te configureren. \033[0m"
 echo
-
-echo
-echo -e '\E[37;44m'"\033[1m *** Restarting the server *** \033[0m"
-echo
-
-# RAILS_ENV=production script/server
-touch tmp/restart.txt
 
 
 
