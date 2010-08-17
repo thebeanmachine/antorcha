9cc0478d8d0d0b0940745387d957f17d276bd9fe
Maarten Brouwers <maarten@brouwers.me>
2010-08-17 12:28:47 +0200

    added @selenium to prevent this feature from running normally

--- features/soap_bibliotheek.feature
+++ features/soap_bibliotheek.feature
@@ -2,5 +2,6 @@ Feature: Soap bibliotheek
   Als ontwikkelaar bij een organisatie wil ik een SOAP bibliotheek, 
 	zodat dit geen belemmering hoeft te vormen bij integratie met andere systemen 
 
+	@selenium
   Scenario: Ik voer het soap test script uit
 		When I execute the SOAP script
--- features/step_definitions/soap_steps.rb
+++ features/step_definitions/soap_steps.rb
@@ -2,19 +2,13 @@ When /^I execute the SOAP script$/ do
   #Beetje vreemde eend in de bijt, maar zo kunnen we soap automatisch testen
   require 'soap/wsdlDriver'
 
+  #always working soap request
   wsdl = 'http://webservices.daehosting.com/services/isbnservice.wso?WSDL'
   driver = SOAP::WSDLDriverFactory.new(wsdl).create_rpc_driver
-
-  # Log SOAP request and response
-  driver.wiredump_file_base = "soap-log.txt"
-
   response =  driver.IsValidISBN13(:sISBN => '9780393068474')
-  #pp(response)
   puts response.isValidISBN13Result
 
   wsdl = 'http://localhost:3000/soap/wsdl'
-
-  driver = SOAP::WSDLDriverFactory.new(wsdl).create_rpc_driver
 
   20.times do 
     start = Time.now
@@ -28,6 +22,4 @@ When /^I execute the SOAP script$/ do
   #puts driver.Show({:username=>"wsdl",:password=>"asdfasdf"},38).body # "asdfasdf" #OK
   #puts driver.Update({:username=>"wsdl",:password=>"asdfasdf"},{:step_id=>6,:body=>"My first WSDL created body",:transaction_id=>30,:title=>"VIS2 #41",:id=>41})
   #puts driver.Deliver({:username=>"wsdl",:password=>"asdfasdf"},{:step_id=>6,:body=>"My first WSDL created body",:transaction_id=>30,:title=>"VIS2 #41",:id=>20})
-
-  #.inspect #FAIL
 end
