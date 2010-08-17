6fb2ae95397ea527145629fc5949edc0f1093f9d
Daan Oosterveld <daan@dusdanig.com>
2010-08-17 16:22:32 +0200

    Added registration explanation page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,27 +23,8 @@ class ApplicationController < ActionController::Base
       flash[:error] = exception.message
       redirect_to messages_path
     else
-      #session should be cleared?
-      render :text => "<p>U staat geregisteerd om te worden geactiveerd</p> TODO!"
+      redirect_to "/registered.html"
     end
-    # render :text => current_user.activated?
-    # if current_user.active?
-    #   flash[:error] = exception.message
-    #   redirect_to messages_path
-    # else      
-    #   render :text => "U ben nog niet geactiveerd. Kom later terug!"
-    # end
-      
-    # if current_user.is_not_registerd_at_all!?
-    #   redirect_to new_user_registration
-    # else
-    #   if current_user.is_a_communicator?
-    #     redirect_to messages_path
-    #   else
-    #     redirect_to silly_page_explaining_registered_users_have_to_be_activated
-    #   end
-    # end
-    # render :text => exception.message
   end
   
 end
