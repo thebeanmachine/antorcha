0f2a6d7d4dca72a17b3d7fc4a5ded37f0b587f94
Maarten Brouwers <maarten@brouwers.me>
2010-08-19 09:10:31 +0200

    tab fixed, login translated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  include SwiftHelper
+  help_can_link_to :user
 
+  
   def index_title
     content_tag :h1, t([:titles, controller.controller_name, :index].join('.'))
   end
