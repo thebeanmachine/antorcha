e8519172f2710e238086cf23db4e0be3074e3bcd
Daan Oosterveld <daan@dusdanig.com>
2010-09-14 12:18:33 +0200

    Fixed ability of maintainer to manage the identity of the antorcha instance.

--- spec/routing/identities_routing_spec.rb
+++ spec/routing/identities_routing_spec.rb
@@ -2,32 +2,21 @@ require 'spec_helper'
 
 describe IdentitiesController do
   describe "routing" do
-    it "recognizes and generates #index" do
-      { :get => "/identities" }.should route_to(:controller => "identities", :action => "index")
-    end
 
     it "recognizes and generates #new" do
-      { :get => "/identities/new" }.should route_to(:controller => "identities", :action => "new")
+      { :get => "/identity/new" }.should route_to(:controller => "identities", :action => "new")
     end
 
     it "recognizes and generates #show" do
-      { :get => "/identities/1" }.should route_to(:controller => "identities", :action => "show", :id => "1")
-    end
-
-    it "recognizes and generates #edit" do
-      { :get => "/identities/1/edit" }.should route_to(:controller => "identities", :action => "edit", :id => "1")
+      { :get => "/identity" }.should route_to(:controller => "identities", :action => "show")
     end
 
     it "recognizes and generates #create" do
-      { :post => "/identities" }.should route_to(:controller => "identities", :action => "create") 
-    end
-
-    it "recognizes and generates #update" do
-      { :put => "/identities/1" }.should route_to(:controller => "identities", :action => "update", :id => "1") 
+      { :post => "/identity" }.should route_to(:controller => "identities", :action => "create") 
     end
 
     it "recognizes and generates #destroy" do
-      { :delete => "/identities/1" }.should route_to(:controller => "identities", :action => "destroy", :id => "1") 
+      { :delete => "/identity" }.should route_to(:controller => "identities", :action => "destroy") 
     end
   end
 end
