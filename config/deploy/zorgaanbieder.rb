6a473f171fadcea566f8f8212b2d06a5bac47578
Daan Oosterveld <daan@dusdanig.com>
2010-09-15 17:03:33 +0200

    Changed the branch for deployment to test to master in stead of maintenance.

--- config/deploy/zorgaanbieder.rb
+++ config/deploy/zorgaanbieder.rb
@@ -4,3 +4,5 @@ set :deploy_to, "/data/app/zorgaanbieder.thebeanmachine.nl"
 role :web, "thebeanmachine.nl"                          # Your HTTP server, Apache/etc
 role :app, "thebeanmachine.nl"                          # This may be the same as your `Web` server
 role :db,  "thebeanmachine.nl", :primary => true # This is where Rails migrations will run
+
+set :branch, 'master'
