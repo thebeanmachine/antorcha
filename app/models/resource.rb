2e63825448e4f2d5ac455b0039e00b99b76511ea
Daan Oosterveld <daan@dusdanig.com>
2010-09-15 16:16:07 +0200

    Removed configuration of resource for testing.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,4 @@
 
 class Resource < HyperactiveResource
   self.site = APP_CONFIG[:olympus_resource]
-  self.site = 'http://tankenberg.heroku.com' if Rails.env.production? and ENV['HOME'] == '/home/daan'
 end
