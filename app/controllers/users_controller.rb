3c678f2a3c4a67f349f72c20c9b6981ba9dbf016
Maarten Brouwers <maarten@brouwers.me>
2010-09-22 18:24:00 +0200

    typos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
     if @user.update_attribute(:user_type, "communicator")
       @user.update_attribute(:activated, true) # Bah.... dit had ik anders kunnen doen :)
-      flash[:notice] = "User is activated"
+      flash[:notice] = "Gebruiker is geactiveerd"
       redirect_to root_path
     else
-      flash[:error] = "User is not activated"
+      flash[:error] = "Deze gebruiker is niet geactiveerd"
       redirect_to users_path
     end
   end
