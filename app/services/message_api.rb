7eee44426dbea7b75e412c29d6cd433bfe1e73c7
Robin Janse <r.janse@thebeanmachine.nl>
2010-09-22 11:32:36 +0200

    Filter unread and unexpired in the messages index. Also implemented Soap commands IndexExpiredUnread, IndexCancelledUnread

--- app/services/message_api.rb
+++ app/services/message_api.rb
@@ -2,7 +2,11 @@
 class MessageAPI < ActionWebService::API::Base
   # api_method :create, :expects => [Api::Token, :string], :returns => [:boolean]
   api_method :show, :expects => [Api::Token, :int], :returns => [Api::Message]
-  api_method :index, :expects => [Api::Token, :string], :returns => [[Api::Message]]
+  api_method :index, :expects => [Api::Token], :returns => [[Api::Message]]
+
+  api_method :index_expired_unread, :expects => [Api::Token], :returns => [[Api::Message]]
+  api_method :index_cancelled_unread, :expects => [Api::Token], :returns => [[Api::Message]]
+  api_method :index_unexpired_unread, :expects => [Api::Token], :returns => [[Api::Message]]
 
   api_method :update, :expects => [Api::Token, Api::Message], :returns => [Api::Message]
 
