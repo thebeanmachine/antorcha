a2eef76f442e07a75f3b1e560aa676086847c22a
Daan Oosterveld <daan@dusdanig.com>
2010-09-22 11:48:14 +0200

    Fixed api message spec, delegation to transaction attributes.

--- spec/services/api/message_spec.rb
+++ spec/services/api/message_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe Api::Message do
+  before(:each) do
+    mock_transaction.as_null_object
+    mock_transaction.stub :cancelled? => false, :expired? => false
+  end
+  
   it "should convert from a Message" do
-    Api::Message.new Message.create
+    Api::Message.new Message.create(:transaction => mock_transaction)
   end
 
   it "should convert from a Message and set the title" do
-    msg = Api::Message.new Message.create(:title => 'aap')
+    msg = Api::Message.new Message.create(:title => 'aap',:transaction => mock_transaction)
     msg.title.should == 'aap'
   end
 
