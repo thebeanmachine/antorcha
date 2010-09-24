270f2e012d0d3c9472315755e4c00ae882156bdc
Daan Oosterveld <daan@dusdanig.com>
2010-09-24 16:28:13 +0200

    No more warnings on sign_in screen.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  skip_before_filter :authenticate_user!
+  
   def index
-    redirect_to messages_url
+    if user_signed_in?
+      redirect_to messages_url
+    else
+      redirect_to new_user_session_url
+    end
   end
 end
