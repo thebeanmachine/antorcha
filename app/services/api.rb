288f078520de023eeb7173fee57ff8150fb96a45
Daan Oosterveld <daan@dusdanig.com>
2010-09-24 12:23:03 +0200

    Action Web Service is unstable in development because not all api classes are explicitly reloaded.
    This issue is only valid if antorcha is run for config.cache_classes = false.
    Marshalling trips over non loaded classes. Action Web Service does not load the api classes.
    Created a more robust version of the update message ability.
    Removed an extra path from the enviroment.

--- app/services/api.rb
+++ app/services/api.rb
@@ -1,3 +1,8 @@
+require 'api/message.rb'
+require 'api/step.rb'
+require 'api/token.rb'
+require 'api/transaction.rb'
+
 
 module Api
 end
--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,7 @@ Rails::Initializer.run do |config|
   # -- all .rb files in that directory are automatically loaded.
 
   # Add additional load paths for your own custom dirs
-  config.load_paths += %W( #{RAILS_ROOT}/app/soap )
+  # config.load_paths += %W( #{RAILS_ROOT}/app/services/api )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
@@ -51,5 +51,6 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   config.i18n.default_locale = :nl
+  
   
 end
