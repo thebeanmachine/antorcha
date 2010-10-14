36dabc69f081a41a4b0872a4678d1896e1aa9d87
Maarten Brouwers <github.com@murb.nl>
2010-10-14 16:34:00 +0200

    soap updated, and removal of old installation files

--- doc/Developers/SOAP/test.rb
+++ doc/Developers/SOAP/test.rb
@@ -1,53 +1,5 @@
     require 'soap/wsdlDriver'
-
-    def startTransaction(step)
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3000/soap/wsdl').create_rpc_driver(nil, "TransactionPort")
-      transaction = driver.Initiate({:username=>"maarten",:password=>"asdfasdf"},step) #OK
-      transaction
-    end
-
-    def deliver(message)
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3000/soap/wsdl').create_rpc_driver(nil, "MessagePort")
-      return driver.Deliver({:username=>"maarten",:password=>"asdfasdf"},message)
-    end
-
-
-    def updateMessage(message)
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3000/soap/wsdl').create_rpc_driver(nil, "MessagePort")
-      message = driver.Update({:username=>"maarten",:password=>"asdfasdf"},message)
-    end
-
-    def index
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3000/soap/wsdl').create_rpc_driver(nil, "MessagePort")
-      driver.Index({:username=>"maarten",:password=>"asdfasdf"}) #OK
-    end
-
-    def indexRead
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3000/soap/wsdl').create_rpc_driver(nil, "MessagePort")
-      return driver.IndexRead({:username=>"maarten",:password=>"asdfasdf"}) #OK
-    end
     
-    def indexUnexpiredUnread
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3010/soap/wsdl').create_rpc_driver(nil, "MessagePort")
-      return driver.IndexUnexpiredUnread({:username=>"maarten",:password=>"asdfasdf"}) #OK      
-    end
-
-    def startSteps()
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3000/soap/wsdl').create_rpc_driver(nil, "StepPort")
-      steps = driver.StartingStepsIndex({:username=>"maarten",:password=>"asdfasdf"}) #OK
-      steps
-    end
-
-    def showMessage(id)
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3000/soap/wsdl').create_rpc_driver(nil, "MessagePort")
-      driver.Show({:username=>"maarten",:password=>"asdfasdf"},id) # "asdfasdf" #OK
-    end
-
-    def deleteMessage(message)
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3000/soap/wsdl').create_rpc_driver(nil, "MessagePort")
-      driver.Delete({:username=>"maarten",:password=>"asdfasdf"},message) # "asdfasdf" #OK
-    end
-
     def testSoap
       #Even testen of SOAP werkt in de basis (uitvoer dient te beginnen met true) 
       wsdl = 'http://webservices.daehosting.com/services/isbnservice.wso?WSDL'
@@ -56,35 +8,104 @@
       puts response.isValidISBN13Result #true
     end
     
-    def quickSend
-      step = startSteps().first
-      driver = SOAP::WSDLDriverFactory.new('http://localhost:3000/soap/wsdl').create_rpc_driver(nil, "MessagePort")
-      message = driver.DeliverMessageAfterInitTransaction({:username=>"maarten",:password=>"asdfasdf"},step,"Titel van quicksend Message","Mijn body") #OK
+    ZORGAANBIEDER = 'http://localhost:3011/soap/wsdl'
+    BUREAUJEUGDZORG = 'http://localhost:3010/soap/wsdl'
+
+    def startTransaction(step, endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "TransactionPort")
+      transaction = driver.Initiate({:username=>"maarten",:password=>"asdfasdf"},step) #OK
+      transaction
+    end
+
+    def deliver(message, endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
+      return driver.Deliver({:username=>"maarten",:password=>"asdfasdf"},message)
+    end
+
+    def reply(message, step, endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
+      return driver.Reply({:username=>"maarten",:password=>"asdfasdf"},message, step)
+    end
+
+    def updateMessage(message, endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
+      message = driver.Update({:username=>"maarten",:password=>"asdfasdf"},message)
+    end
+
+    def index(endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
+      driver.Index({:username=>"maarten",:password=>"asdfasdf"}) #OK
+    end
+
+    def indexRead(endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
+      return driver.IndexRead({:username=>"maarten",:password=>"asdfasdf"}) #OK
+    end
+    
+    def indexUnexpiredUnread(endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
+      return driver.IndexUnexpiredUnread({:username=>"maarten",:password=>"asdfasdf"}) #OK      
+    end
+
+    def startSteps(endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "StepPort")
+      steps = driver.StartingStepsIndex({:username=>"maarten",:password=>"asdfasdf"}) #OK
+      steps
+    end
+
+    def effectStepsIndex(message, endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "StepPort")
+      steps = driver.EffectStepsIndex({:username=>"maarten",:password=>"asdfasdf"}, message) #OK
+      steps
+    end
+    
+    def showMessage(id,endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
+      driver.Show({:username=>"maarten",:password=>"asdfasdf"},id) # "asdfasdf" #OK
+    end
+
+    def deleteMessage(message,endpoint)
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
+      driver.Delete({:username=>"maarten",:password=>"asdfasdf"},message) # "asdfasdf" #OK
+    end
+    
+    def deliverMessageAfterInitTransaction(titel, body, endpoint)
+      step = startSteps(endpoint).first
+      driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
+      message = driver.DeliverMessageAfterInitTransaction({:username=>"maarten",:password=>"asdfasdf"},step,titel,body) #OK
       puts "Bericht verstuurd met bericht id #{message.id}"
       puts message.inspect
     end
 
-    # testSoap 
-    # quickSend
-    # step = startSteps.first
-    # message = startTransaction(step)
-    # 
-    # puts showMessage(message.id).title
-    # message.title = "updated title"
-    # message.body = "updated title"
-    # updateMessage(message)
-    # 
-    # message = deliver(message)
-    # puts message.inspect
-    # 
-    # message = startTransaction(step)
-    # message.title = "delete me"
-    # updateMessage(message)
-    # puts "Alle berichten"
-    # index.each do |m| puts m.title end
-    # #deleteMessage(message) #Geen authorisatie
+    testSoap 
+    deliverMessageAfterInitTransaction("snelverzonden","<?xml version=\"1.0\" encoding=\"UTF-8\"?><melding>tadu tadu tadu</melding>",ZORGAANBIEDER)
+    step = startSteps(ZORGAANBIEDER).first
+    message = startTransaction(step,ZORGAANBIEDER)
+    
+    puts showMessage(message.id,ZORGAANBIEDER).title
+    
+    message.title = "Ik maak een handmatige melding melding"
+    message.body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><melding>updated title</melding>"
+    
+    updateMessage(message,ZORGAANBIEDER)
+    
+    deliver(message,ZORGAANBIEDER)
+    
+    index(BUREAUJEUGDZORG).each do |m| puts m.title end
+    messageToReplyTo = indexUnexpiredUnread(BUREAUJEUGDZORG).first
+    replysteps = effectStepsIndex(messageToReplyTo, BUREAUJEUGDZORG)
+    replysteps.each do |s|
+      puts s.title
+    end
+    replymessage = reply(messageToReplyTo,replysteps.first,BUREAUJEUGDZORG)
+    replymessage.title = "Bedankt voor uw melding"
+    replymessage.body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><melding>updated title</melding>"
+    replymessage = updateMessage(replymessage, BUREAUJEUGDZORG)
+    deliver(replymessage, BUREAUJEUGDZORG)
+    
+    # deleteMessage(message) #Geen authorisatie
     # puts "Alleen gelezen berichten"
     # indexRead.each do |m| puts m.title end
-    #     
+    #      
 
-    indexUnexpiredUnread.each do |m| puts m.title end
+    #indexUnexpiredUnread.each do |m| puts m.title end
