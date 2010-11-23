e3833949d2d8fc7c6c1eea3d9ca4ef2de12193f8
Robin Janse <r.janse@thebeanmachine.nl>
2010-11-23 11:55:54 +0100

    Manage user roles spec'd and refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,17 +20,10 @@ class UsersController < ApplicationController
       end
     elsif @user.user_type == "communicator" || @user.user_type == "maintainer"
       @user.castables.destroy_all
-      if params[:roles]        
-        params[:roles].each do |role|
-          if @user.castables.build(:role_id => role).save # Op id want dat scheelt een query
-            flash[:notice] = "Rollen zijn gewijzigd voor #{@user.username}"
-          else
-            flash[:error] = "Rollen zijn ongewijzigd"
-          end 
-        end
-      end
+      @user.cast(params[:roles]) if params[:roles]
+      flash[:notice] = "Rollen zijn gewijzigd"
     else
-      flash[:notice] = "Gebruiker is onbekend"
+      flash[:error] = "Gebruiker is onbekend"
     end
     
     redirect_to users_path
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,9 +2,52 @@ require 'spec_helper'
 
 describe UsersController do
 
-  #Delete this example and add some real ones
-  it "should use UsersController" do
-    controller.should be_an_instance_of(UsersController)
+  include Devise::TestHelpers
+
+  before(:each) do
+    turn_of_devise_and_cancan_because_this_is_specced_in_the_ability_spec
+  end
+  
+  specify { should have_devise_before_filter }
+
+  context "with PUT update" do
+    
+    context "and a registerd user but not yet a comunicator or a maintainer role" do
+           
+      it "should flash notice 'Gebruiker is geactiveerd'" do
+        mock_user(:inactive).stub :user_type => "registered", :activated => false
+        mock_user(:inactive).stub :update_attribute => "communicator"          
+        User.should_receive(:find).with("34").and_return(mock_user(:inactive))         
+        put :update, :id => "34"      
+        flash[:notice].should == "Gebruiker is geactiveerd"        
+        response.should redirect_to(users_path)
+      end
+      
+      it "should flash error 'WHOEPS!'" do
+        mock_user(:inactive).stub :user_type => "registered", :activated => false
+        mock_user(:inactive).stub :update_attribute
+        mock_user(:inactive).errors.stub :full_messages => %w(WHOEPS!)          
+        User.should_receive(:find).with("34").and_return(mock_user(:inactive))         
+        put :update, :id => "34"              
+        flash[:error].should == mock_user(:inactive).errors.full_messages        
+        response.should redirect_to(users_path)
+      end
+      
+    end
+    
+    context "and already a communicator or a maintainer role" do
+      it "should cast new roles the specific user" do
+        mock_user(:active).stub :user_type => "communicator", :activated => true, :castables => mock_castables
+        mock_user(:active).stub(:cast)
+        mock_castables.stub :destroy_all
+        
+        User.should_receive(:find).with("34").and_return(mock_user(:active))
+        put :update, :id => "34", :roles => [1,2,3]
+        
+        flash[:notice].should == "Rollen zijn gewijzigd"
+        response.should redirect_to users_path
+      end
+    end
   end
 
 end
