6b6d9594b46fcc530732413aa9407631d9fb9cce
Robin Janse <r.janse@thebeanmachine.nl>
2011-02-01 14:52:41 +0100

    Soap delivermessageafterinittransaction test

--- doc/Developers/SOAP/test.rb
+++ doc/Developers/SOAP/test.rb
@@ -8,88 +8,111 @@
       puts response.isValidISBN13Result #true
     end
     
-    ZORGAANBIEDER = 'http://maarten:asdfasdf@localhost:3011/soap/wsdl'
-    BUREAUJEUGDZORG = 'http://maarten:asdfasdf@localhost:3011/soap/wsdl'
+    ZORGAANBIEDER = 'http://henk:qwerty@localhost:3000/soap/wsdl'
+    BUREAUJEUGDZORG = 'http://henk:qwertyf@localhost:3000/soap/wsdl'
+    # ZORGAANBIEDER = 'http://maarten:asdfasdf@localhost:3011/soap/wsdl'
+    # BUREAUJEUGDZORG = 'http://maarten:asdfasdf@localhost:3011/soap/wsdl'
     #ZORGAANBIEDER = 'http://maarten:asdfasdf@zorgaanbieder.thebeanmachine.nl/soap/wsdl'
     #BUREAUJEUGDZORG = 'http://maarten:asdfasdf@zorgaanbieder.thebeanmachine.nl/soap/wsdl'
 
     
     def startTransaction(step, endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "TransactionPort")
-      transaction = driver.Initiate({:username=>"maarten",:password=>"asdfasdf"},step) #OK
+      transaction = driver.Initiate({:username=>"henk",:password=>"qwerty"},step) #OK
       transaction
     end
 
     def deliver(message, endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
-      return driver.Deliver({:username=>"maarten",:password=>"asdfasdf"},message)
+      return driver.Deliver({:username=>"henk",:password=>"qwerty"},message)
     end
 
     def reply(message, step, endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
-      return driver.Reply({:username=>"maarten",:password=>"asdfasdf"},message, step)
+      return driver.Reply({:username=>"henk",:password=>"qwerty"},message, step)
     end
 
     def updateMessage(message, endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
-      message = driver.Update({:username=>"maarten",:password=>"asdfasdf"},message)
+      message = driver.Update({:username=>"henk",:password=>"qwerty"},message)
     end
 
     def index(endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
-      driver.Index({:username=>"maarten",:password=>"asdfasdf"}) #OK
+      driver.Index({:username=>"henk",:password=>"qwerty"}) #OK
     end
 
     def indexRead(endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
-      return driver.IndexRead({:username=>"maarten",:password=>"asdfasdf"}) #OK
+      return driver.IndexRead({:username=>"henk",:password=>"qwerty"}) #OK
     end
     
     def indexUnexpiredUnread(endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
-      return driver.IndexUnexpiredUnread({:username=>"maarten",:password=>"asdfasdf"}) #OK      
+      return driver.IndexUnexpiredUnread({:username=>"henk",:password=>"qwerty"}) #OK      
     end
 
     def startSteps(endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "StepPort")
-      steps = driver.StartingStepsIndex({:username=>"maarten",:password=>"asdfasdf"}) #OK
+      steps = driver.StartingStepsIndex({:username=>"henk",:password=>"qwerty"}) #OK
       steps
     end
 
     def effectStepsIndex(message, endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "StepPort")
-      steps = driver.EffectStepsIndex({:username=>"maarten",:password=>"asdfasdf"}, message) #OK
+      steps = driver.EffectStepsIndex({:username=>"henk",:password=>"qwerty"}, message) #OK
       steps
     end
     
     def showMessage(id,endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
-      driver.Show({:username=>"maarten",:password=>"asdfasdf"},id) # "asdfasdf" #OK
+      driver.Show({:username=>"henk",:password=>"qwerty"},id) # "qwerty" #OK
     end
 
     def deleteMessage(message,endpoint)
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
-      driver.Delete({:username=>"maarten",:password=>"asdfasdf"},message) # "asdfasdf" #OK
+      driver.Delete({:username=>"henk",:password=>"qwerty"},message) # "qwerty" #OK
     end
     
     def deliverMessageAfterInitTransaction(titel, body, endpoint)
       step = startSteps(endpoint).first
       driver = SOAP::WSDLDriverFactory.new(endpoint).create_rpc_driver(nil, "MessagePort")
-      message = driver.DeliverMessageAfterInitTransaction({:username=>"maarten",:password=>"asdfasdf"},step,titel,body) #OK
-      puts "Bericht verstuurd met bericht id #{message.id}"
-      puts message.inspect
+      @message = driver.DeliverMessageAfterInitTransaction({:username=>"henk",:password=>"qwerty"},step,titel,body) #OK
+      # puts "Bericht verstuurd met bericht id #{message.id}"
+      # puts message.inspect
     end
 
-    testSoap 
-    deliverMessageAfterInitTransaction("snelverzonden","<?xml version=\"1.0\" encoding=\"UTF-8\"?><melding>tadu tadu tadu</melding>",ZORGAANBIEDER)
-    step = startSteps(ZORGAANBIEDER).first
-    message = startTransaction(step,ZORGAANBIEDER)
+    def stresstest number     
+      @title = "Start 'deliverMessageAfterInitTransaction' SOAP-call stresstest."
+      p @title
+
+      number.times do |i|
+        p "-" * @title.size
+        p "soapcall #{i+1}: "
+        deliverMessageAfterInitTransaction("snelverzonden","<?xml version=\"1.0\" encoding=\"UTF-8\"?><melding>tadu tadu tadu</melding>",ZORGAANBIEDER)
+        p @message.body.empty? ? "NOT" : "OK"
+      end
+      
+    end
+
+    #testSoap 
+    stresstest 100
+    
+    
+    
+    # step = startSteps(ZORGAANBIEDER).first
+    # message = startTransaction(step,ZORGAANBIEDER)
+    
+    
     # 
-    puts showMessage(message.id,ZORGAANBIEDER).title
-    message.title = "Ik maak een handmatige melding melding"
-    message.body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><melding>updated title</melding>"
-    updateMessage(message,ZORGAANBIEDER)
-    deliver(message,ZORGAANBIEDER)
+    # puts showMessage(message.id,ZORGAANBIEDER).title
+    # message.title = "Ik maak een handmatige melding melding"
+    # message.body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><melding>updated title</melding>"
+    # updateMessage(message,ZORGAANBIEDER)
+    # deliver(message,ZORGAANBIEDER)
+    
+    
+    
     # 
     # index(BUREAUJEUGDZORG).each do |m| puts m.title end
     # messageToReplyTo = indexUnexpiredUnread(BUREAUJEUGDZORG).first
