432410a3ee3cfad0b916e4721968b8ea6cde8c72
unknown <nagel@.thorax.local>
2011-08-17 14:49:29 +0200

    Batch-bestanden voorzien van Titel in command line (voor hoe lang het nog duurt tot de service er is)

--- start.bat
+++ start.bat
@@ -4,5 +4,6 @@ echo *** Antorcha starten ***
 echo.
 echo  Antorcha wordt gestart in productie modus
 echo.
+TITLE Antorcha Server
 set RAILS_ENV=production
 call ruby script/server
--- start_engine.bat
+++ start_engine.bat
@@ -4,5 +4,6 @@ echo *** Antorcha Engine starten ***
 echo.
 echo  De Antorcha engine wordt gestart in productie modus
 echo.
+TITLE Antorcha Engine
 set RAILS_ENV=production
 ruby script/delayed_job run
